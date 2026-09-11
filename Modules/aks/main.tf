0655bee096549504b9bde1abb5357de0ba8e65ea
Vinav30 <vinav30@outlook.com>
2026-09-11 14:35:50 +0530

    vm size updated

--- Modules/aks/main.tf
+++ Modules/aks/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size = "Standard_D2_v6"
+    vm_size = "Standard_D2ds_v6"
     }
 
   identity {
